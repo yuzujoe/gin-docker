82fc283ac1eff781bd4b6b2d02ffca3ad8d9ca42
ohira yuzuru <yuzu-joe@ichain.co.jp>
2019-08-19 16:31:51 +0900

    connect mysql

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # gin-docker
+
+## あとで使用
+db.AutoMigrate(&Message{})
+	db.Create(&Message{Message: "Helloworld"})
+type Message struct {
+	gorm.Model
+	Message string
+}
--- src/api/main.go
+++ src/api/main.go
@@ -1,8 +1,20 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"database/sql"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+var DbConnect *sql.DB
 
 func main() {
+	DbConnect, err := sql.Open("mysql", "root:@tcp(db:3306)/godocker")
+	if err != nil {
+		panic(err.Error())
+	}
+	DbConnect.Close()
 	r := gin.Default()
 	r.GET("/hello", func(c *gin.Context) {
 		c.JSON(200, gin.H{
